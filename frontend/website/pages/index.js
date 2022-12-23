30a051642ee935c9bbbbc408968574e8ba8ebb64
islandskan <sigridureggertsdottir.art@gmail.com>
2022-12-23 16:29:27 +0100

    import Footer and fix path name

--- frontend/website/pages/index.js
+++ frontend/website/pages/index.js
@@ -1,10 +1,15 @@
 import { MetaData } from '../components/MetaData';
 import { PageContent } from '../components/pagecontent';
+import { PageFooter } from '../components/Footer';
+import { Menubar } from '../components/Menubar';
+
 function HomePage() {
     return (
         <>
             <MetaData page='Home' />
+            <Menubar />
             <PageContent />
+            <PageFooter />
         </>
     );
 }
